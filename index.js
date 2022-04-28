3880910eeda68130759a58a80ee446ac2170d7a4
Josep Servat <jservat@outlook.es>
2022-04-28 21:12:07 +0200

    fetch initial data·

---
+++ index.js
@@ -0,0 +1,40 @@
+const BASE_URL = 'https://rickandmortyapi.com/api'
+
+
+const init = async () => {
+  const characters = await getCharacters();
+  console.log(characters);
+  parseData(characters);
+}
+
+window.onload = () => {
+  init();
+}
+
+const getCharacters = async () => {
+  const data = await fetch(`${BASE_URL}/character`);
+  const dataToJson = await data.json();
+  return dataToJson;
+}
+
+const printCharacter = (character) => {
+  const characterContainer = document.querySelector('#characters');
+  characterContainer.innerHTML += `
+    <li>
+      <h2>${character.name}</h2>
+      <img src=${character.image} alt=${character.name}>
+      <h3>${character.location}</h3>
+    </li>
+  `
+}
+
+const parseData = (data) => {
+  data.results.forEach((character) => {
+    return  printCharacter({
+      name: character.name,
+      image: character.image,
+      location: character.location.name + '' + (character.location.origin || ''),
+      orign: character.origin
+    })
+  })
+}
